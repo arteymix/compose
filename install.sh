bafc1df42db0ac2c128b7777e7ef8e4c88ed15ce
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2017-10-27 17:48:35 -0400

    Use Meson 0.43 and use '_dep' suffix

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-mkdir -p ${MESON_INSTALL_DESTDIR_PREFIX}/include/compose-1.0
-install ${MESON_BUILD_ROOT}/src/compose.h ${MESON_INSTALL_DESTDIR_PREFIX}/include/compose-1.0
-
-mkdir -p ${MESON_INSTALL_DESTDIR_PREFIX}/share/vala/vapi
-install ${MESON_BUILD_ROOT}/src/compose-1.0.vapi ${MESON_INSTALL_DESTDIR_PREFIX}/share/vala/vapi
